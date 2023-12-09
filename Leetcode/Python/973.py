5a336a61e4a825f1fb9988ff452ef689e4e1646c
vuhnger <victor.uhnger@gmail.com>
2023-12-09 20:02:14 +0100

    improved runtime on 973

--- Leetcode/Python/973.py
+++ Leetcode/Python/973.py
@@ -1,4 +1,4 @@
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
 import math
 
 class Solution(object):
@@ -8,11 +8,6 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        heap = []
-        for x, y in points:
-            distance = sqrt(x**2 + y**2)
-            heappush(heap, (distance, (x,y)))
-        res = []
-        for i in range(k):
-            res.append(heappop(heap)[1])
-        return res
+        heap = [(x**2 + y**2, (x,y)) for x,y in points]
+        heapify(heap)
+        return [heappop(heap)[1] for _ in range(k)]
