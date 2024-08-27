839287a0c51770408c5335ed60ee9cd162593d22
vuhger <victor.uhnger@gmail.com>
2024-08-27 18:42:43 +0200

    solved shortcutttowhat

---
+++ Kattis/shortcuttowhat.py
@@ -0,0 +1,7 @@
+"""
+Write a program that reads an integer 
+ from the input. The program should add five to 
+, then triple the value and finally subtract ten. You are welcome to simplify the formula if you can, as long as the program gives the correct results.
+"""
+
+print(((int(input())+5)*3)-10)
